65cc082884481398102d4ee9eeb7cc0fff2ca17f
RubyBlumel <rkblumel@gmail.com>
2026-01-03 19:36:38 -0500

    Update Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -3,12 +3,10 @@ def hello_world():
 
 
 def input_output():
-    name = input("what is your name")
-    age = int(input("how old are you"))
-    height = float(input("how tall are you"))
+    name = input()
+    age = int(input())
+    height = float(input())
 
-    print(
-        "your name is " + name + "\n"
-        "and you are " + str(age) + " years old\n"
-        "you are " + str(height) + " feet tall"
-    )
+    print("your name is: " + name)
+    print("your age is: " + str(age))
+    print("your height is: " + str(height))
