e69067b56e7657787c5fba7446d65b24543139a5
RubyBlumel <rkblumel@gmail.com>
2026-01-03 19:03:38 -0500

    Update Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -1,16 +1,17 @@
-# Function 1: Write a simple Hello World program
 def hello_world():
     print("Hello, World!")
 
 
-# Function 2: Get input and output with different variable types
 def input_output():
-    name = input("What is your name? ")
-    age = int(input("How old are you? "))
-    height = float(input("How tall are you in feet? "))
+    name = input()
+    age = int(input())
+    height = float(input())
 
-    print(f"Your name is {name}")
-    print(f"You are {age} years old")
-    print(f"You are {height} feet tall")
+    output = (
+        f"Your name is {name}\n"
+        f"You are {age} years old\n"
+        f"You are {height} feet tall"
+    )
 
-
+    print(output)
+    return output
